c4da5c2b84bb9d2e812ea68d0f0c54121e52195d
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2025-09-05 03:14:16 -0700

    login screen updated

--- src/screens/Auth/LoginScreen.js
+++ src/screens/Auth/LoginScreen.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import AntIcon from "react-native-vector-icons/AntDesign";
 import {
   View,
   Text,
@@ -14,51 +15,80 @@ const { width } = Dimensions.get("window");
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false); 
 
   const handleLogin = () => {
     console.log("Email:", email, "Password:", password);
-    navigation.replace("Home"); // Later connect with SQLite
+    navigation.replace("Home"); 
   };
 
   return (
     <View style={styles.container}>
-      {/* Top full-width image */}
+    
       <Image
         source={require("../../assets/Group-118.png")}
         style={styles.topImage}
         resizeMode="cover"
       />
 
-      {/* Login content */}
-      <View style={styles.content}>
-        <Text style={styles.title}>Welcome Back!</Text>
-        <Text style={styles.subtitle}>Login to continue</Text>
+      <View style={{ flexDirection: "row", justifyContent: "center", marginVertical: 15 }}>
+        <Image
+          source={require("../../assets/Group-6.png")}
+          style={{ width: 125, height: 100, marginHorizontal: 10 }}
+          resizeMode="contain"
+        />
+      </View>
 
+      <Text style={{ textAlign: "center", color: "#000", fontSize: 16, marginBottom: 10 }}>
+        or login with email
+      </Text>
+
+      <View style={styles.content}>
+      
+        <Text style={{ alignSelf: "flex-start", marginBottom: 5, color: "#333", fontSize: 14 }}>
+          Email:
+        </Text>
         <TextInput
           style={styles.input}
-          placeholder="Email"
-          placeholderTextColor="#666"
           value={email}
           onChangeText={setEmail}
+          placeholder="Enter your email"
+          keyboardType="email-address"
         />
 
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor="#666"
-          secureTextEntry
-          value={password}
-          onChangeText={setPassword}
-        />
+        
+        <Text style={{ alignSelf: "flex-start", marginBottom: 5, color: "#333", fontSize: 14 }}>
+          Password:
+        </Text>
+        <View style={{ width: "100%", position: "relative" }}>
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Enter your password"
+            placeholderTextColor="#666"
+            secureTextEntry={!showPassword} 
+          />
 
+          
+          <TouchableOpacity
+            style={{ position: "absolute", right: 15, top: 10 }}
+            onPress={() => setShowPassword(!showPassword)}
+          >
+            <AntIcon name={showPassword ? "eyeo" : "eye"} size={20} color="#221f1fff" />
+          </TouchableOpacity>
+        </View>
+
+       
         <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-          <Text style={styles.loginButtonText}>Login</Text>
+          <Text style={styles.loginButtonText}>Sign in</Text>
         </TouchableOpacity>
 
+   
         <View style={styles.registerRow}>
           <Text style={styles.registerText}>Don’t have an account?</Text>
           <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-            <Text style={styles.registerLink}> Sign up</Text>
+            <Text style={styles.registerLink}>Register</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -73,24 +103,13 @@ const styles = StyleSheet.create({
   },
   topImage: {
     width: width,
-    height: 250, // Covers top area of screen
+    height: 250,
   },
   content: {
     flex: 1,
     alignItems: "center",
     paddingHorizontal: 24,
     paddingTop: 20,
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: "bold",
-    color: "#000",
-    marginBottom: 5,
-  },
-  subtitle: {
-    fontSize: 15,
-    color: "#555",
-    marginBottom: 25,
   },
   input: {
     width: "100%",
@@ -127,6 +146,6 @@ const styles = StyleSheet.create({
   registerLink: {
     fontSize: 14,
     fontWeight: "bold",
-    color: "#FFD700",
+    color: "#1a1918ff",
   },
 });
