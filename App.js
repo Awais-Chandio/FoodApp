9c7d5875a6e406dabb70dd690f7ce92dacddebbb
Muhammad Awais <94171625+Awais-Chandio@users.noreply.github.com>
2025-10-06 10:24:48 -0700

    secrets updated

--- App.js
+++ App.js
@@ -1,27 +1,3 @@
-// import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import AppNavigator from "./src/navigation/AppNavigator";
-// import { useCreateTables } from "./src/database/dbs";
-// import { AuthProvider } from "./src/screens/Auth/AuthContext";
-
-
-// const App=()=> {
-  
-//   useCreateTables(); 
-
-//   return (
-//     <AuthProvider>
-//       <NavigationContainer>
-//       <AppNavigator />
-//     </NavigationContainer>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App
-
-
-
 import React from "react";
 import { NavigationContainer} from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
