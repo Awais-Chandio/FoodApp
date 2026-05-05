4ada57749a957f649027889cb4c89cd717eb593c
Muhammad Awais <awaischandio851@gmail.com>
2026-05-05 23:05:07 +0500

    initialized in App.js

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { useCreateTables } from "./src/database/dbs";
 import { AuthProvider } from "./src/screens/Auth/AuthContext";
 import Toast from "react-native-toast-message";
 import { ThemeProvider } from "./src/Context/ThemeProvider";
+import notificationService from "./src/services/notificationService";
 
 const App = () => {
   useCreateTables();
+
+  useEffect(() => {
+    // Initialize notification service
+    notificationService.initialize();
+
+    // Cleanup on unmount
+    return () => {
+      notificationService.cleanup();
+    };
+  }, []);
 
   return (
     <ThemeProvider>
